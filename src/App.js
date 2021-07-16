ea1d464c9fd8de88ec610ee2c72e8fe6bffa8283
Nidhi7430 <nidhipatel439@gmail.com>
2021-07-16 19:50:49 +0530

    changes in index&App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import Header from "./components/header/Header";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Shop from "./components/shop/Shop";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
@@ -15,32 +10,30 @@ import "./App.css";
 function App() {
   return (
     <>
-      <Router>
-        <Header />
-        <div className="main_top_padding">
-          <Switch>
-            <Route exact path="/Home">
-              <Home />
-            </Route>
-            <Route exact path="/Shop">
-              <Shop />
-            </Route>
-            <Route exact path="/About">
-              <About />
-            </Route>
-            <Route exact path="/Contact">
-              <Contact />
-            </Route>
-            <Route exact path="/">
-              <Redirect to="/Home" />
-            </Route>
-            <Route exact path="*">
-              <Redirect to="/Home" />
-            </Route>
-          </Switch>
-        </div>
-        <Footer />
-      </Router>
+      <Header />
+      <div className="main_top_padding">
+        <Switch>
+          <Route exact path="/Home">
+            <Home />
+          </Route>
+          <Route exact path="/Shop">
+            <Shop />
+          </Route>
+          <Route exact path="/About">
+            <About />
+          </Route>
+          <Route exact path="/Contact">
+            <Contact />
+          </Route>
+          <Route exact path="/*">
+            <Redirect to="/Home" />
+          </Route>
+          {/* <Route exact path="*">
+            <Redirect to="/Home" />
+          </Route> */}
+        </Switch>
+      </div>
+      <Footer />
     </>
   );
 }
