d505d0e23c700b6a9feccca7525b2922893c3ebe
Nidhi7430 <nidhipatel439@gmail.com>
2021-09-08 13:21:19 +0530

    set login and signup modal

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -15,6 +15,13 @@ import {
   MDBDropdownItem,
   MDBDropdownLink,
   MDBBtn,
+  MDBModal,
+  MDBModalDialog,
+  MDBModalContent,
+  MDBModalHeader,
+  MDBModalTitle,
+  MDBModalBody,
+  MDBInput,
 } from "mdb-react-ui-kit";
 import "./header.css";
 import logo from "../../images/logo.png";
@@ -26,6 +33,10 @@ const Header = () => {
     showPath(window.location.pathname);
   }, []);
   // const path = window.location.pathname;
+
+  const [toggleOneModal, setToggleOneModal] = useState(false);
+  const [toggleTwoModal, setToggleTwoModal] = useState(false);
+
   return (
     <MDBNavbar
       expand="lg"
@@ -36,10 +47,6 @@ const Header = () => {
     >
       <MDBContainer>
         <MDBNavbarBrand href="\Home">
-          {/* <h3 className="fw-bold logo">
-            <span className="logo_f_letter">T</span>oys
-            <span className="logo_s_letter">U</span>niverse
-          </h3> */}
           <img src={logo} alt="logo" className="mw-100 header_logo" />
         </MDBNavbarBrand>
         <MDBNavbarToggler
@@ -101,14 +108,135 @@ const Header = () => {
               </MDBNavbarLink>
             </MDBNavbarItem>
             <MDBNavbarItem>
-              <MDBNavbarLink href="#!">
-                <MDBBtn color="info">SignIn</MDBBtn>
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href="#!">
-                <MDBBtn color="info">Other</MDBBtn>
-              </MDBNavbarLink>
+              <MDBBtn
+                onClick={() => setToggleOneModal(!toggleOneModal)}
+                color="info"
+              >
+                Login
+              </MDBBtn>
+              <MDBModal
+                show={toggleOneModal}
+                getOpenState={(e: any) => setToggleOneModal(e)}
+                tabIndex="-1"
+              >
+                <MDBModalDialog centered>
+                  <MDBModalContent>
+                    <MDBModalHeader>
+                      <MDBModalTitle>Login</MDBModalTitle>
+                      <MDBBtn
+                        className="btn-close"
+                        color="none"
+                        onClick={() => setToggleOneModal(!toggleOneModal)}
+                      ></MDBBtn>
+                    </MDBModalHeader>
+                    <MDBModalBody>
+                      <form>
+                        <MDBInput
+                          label="Type your email"
+                          group
+                          type="email"
+                          validate
+                          error="wrong"
+                          success="right"
+                          className="my-4"
+                        />
+                        <MDBInput
+                          label="Type your password"
+                          group
+                          type="password"
+                          validate
+                          className="my-4"
+                        />
+                        <p className="font-small d-flex justify-content-end">
+                          Forgot
+                          <a
+                            href="#!"
+                            className="text-info font-weight-bold ms-1"
+                          >
+                            Password?
+                          </a>
+                        </p>
+                        <div className="text-center ">
+                          <MDBBtn color="info">Login</MDBBtn>
+                        </div>
+                        <p className="font-small d-flex justify-content-center mt-3">
+                          Don't have an account?
+                          <a
+                            onClick={() => {
+                              setToggleOneModal(!toggleOneModal);
+                              setTimeout(() => {
+                                setToggleTwoModal(!toggleTwoModal);
+                              }, 400);
+                            }}
+                            href="#!"
+                            className="text-info font-weight-bold ms-1"
+                          >
+                            Sign up
+                          </a>
+                        </p>
+                      </form>
+                    </MDBModalBody>
+                  </MDBModalContent>
+                </MDBModalDialog>
+              </MDBModal>
+
+              <MDBModal
+                show={toggleTwoModal}
+                getOpenState={(e: any) => setToggleTwoModal(e)}
+                tabIndex="-1"
+              >
+                <MDBModalDialog centered>
+                  <MDBModalContent>
+                    <MDBModalHeader>
+                      <MDBModalTitle>Sign Up</MDBModalTitle>
+                      <MDBBtn
+                        className="btn-close"
+                        color="none"
+                        onClick={() => setToggleTwoModal(!toggleTwoModal)}
+                      ></MDBBtn>
+                    </MDBModalHeader>
+                    <MDBModalBody>
+                      <MDBInput
+                        label="Your name"
+                        group
+                        type="text"
+                        validate
+                        error="wrong"
+                        success="right"
+                        className="my-4"
+                      />
+                      <MDBInput
+                        label="Your email"
+                        group
+                        type="email"
+                        validate
+                        error="wrong"
+                        success="right"
+                        className="my-4"
+                      />
+                      <MDBInput
+                        label="Confirm your email"
+                        group
+                        type="text"
+                        validate
+                        error="wrong"
+                        success="right"
+                        className="my-4"
+                      />
+                      <MDBInput
+                        label="Your password"
+                        group
+                        type="password"
+                        validate
+                        className="my-4"
+                      />
+                      <div className="text-center ">
+                        <MDBBtn color="info">Sign Up</MDBBtn>
+                      </div>
+                    </MDBModalBody>
+                  </MDBModalContent>
+                </MDBModalDialog>
+              </MDBModal>
             </MDBNavbarItem>
             <MDBNavbarItem>
               <MDBDropdown>
